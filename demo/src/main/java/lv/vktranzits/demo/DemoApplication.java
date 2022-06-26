950d2e033560247a5c06cc3ca7b0723fa5cbd8c6
SynxPlays <synxplays@inbox.lv>
2022-06-26 11:56:47 +0300

    Add: Bean & App repos

--- demo/src/main/java/lv/vktranzits/demo/DemoApplication.java
+++ demo/src/main/java/lv/vktranzits/demo/DemoApplication.java
@@ -1,13 +1,36 @@
 package lv.vktranzits.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import lv.vktranzits.demo.repos.*;
 
 @SpringBootApplication
+@EnableJpaRepositories("lv.vktranzits.demo.*")
+@ComponentScan(basePackages = { "lv.vktranzits.demo.*" })
+@EntityScan("lv.vktranzits.demo.*")
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner runner(ICompanyRepo companyRepo, ICourseCalendarRepo courseCalRepo, ICourseTypeRepo courseTypeRepo, IPositionRepo posRepo)
+	{
+
+		return new CommandLineRunner() {
+
+			@Override
+			public void run(String... args) throws Exception {
+
+			}
+		};
 	}
 
 }
