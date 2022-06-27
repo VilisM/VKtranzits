f27668e73334f295f937c304b954723aca0b4d45
SynxPlays <synxplays@inbox.lv>
2022-06-28 00:31:25 +0300

    Fix: Errors?

--- demo/src/main/java/lv/vktranzits/demo/repos/IEmployeeCourseRepo.java
+++ demo/src/main/java/lv/vktranzits/demo/repos/IEmployeeCourseRepo.java
@@ -3,6 +3,7 @@ package lv.vktranzits.demo.repos;
 import org.springframework.data.repository.CrudRepository;
 
 import lv.vktranzits.demo.models.Course;
+import lv.vktranzits.demo.models.EmployeeCourse;
 
 public interface IEmployeeCourseRepo extends CrudRepository<EmployeeCourse , Integer> {
 
