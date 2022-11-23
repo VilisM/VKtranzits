1beed4468da85359e5bde2c6d7ec8687fbc1fce4
VilisM <thedkz1@gmail.com>
2022-11-23 16:02:59 +0200

    Fix: Typo mistake

--- demo/src/main/java/lv/vktranzits/demo/config/InterConfig.java
+++ demo/src/main/java/lv/vktranzits/demo/config/InterConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -14,32 +15,31 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-@Configurable
+@Configuration
 @ComponentScan(basePackages = "com.baeldung.internationalization.config")
 public class InterConfig implements WebMvcConfigurer {
     
     @Bean("messageSource")
 	public MessageSource messageSource() {
 	    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-	    messageSource.setBasenames("classpath:language/messages");
+	    messageSource.setBasenames("language/messages");
 	    messageSource.setDefaultEncoding("UTF-8");
 	    return messageSource;
 	}
 	
 	@Bean
-	public LocaleResolver localeResolver() {
-	    SessionLocaleResolver slr = new SessionLocaleResolver();
-	    slr.setLocaleAttributeName("current.locale");
-	    slr.setTimeZoneAttributeName("current.timezone");
-	    return slr;
-	}
+    public LocaleResolver localeResolver(){
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.US);
+        return sessionLocaleResolver;
+    }
 	
 	@Bean
-	public LocaleChangeInterceptor localeChangeInterceptor() {
-	    LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
-	    localeChangeInterceptor.setParamName("lang");
-	    return localeChangeInterceptor;
-	}
+    LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
 	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
